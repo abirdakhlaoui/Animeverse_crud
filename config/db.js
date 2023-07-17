bf4cd75e46f04f5f1a014e47ae0c8f4ca319a251
abir dakhlaoui <abyrdakhlaoui2000@gmail.com>
2023-07-17 10:52:06 +0100

    le backend de projet

--- config/db.js
+++ config/db.js
@@ -4,6 +4,6 @@ const mongoose =require("mongoose")
 const DB_NAME="ecom-animverse"
 
 
-mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`)
+mongoose.connect(`mongodb+srv://abyrdakhlaoui:SaV6Zvr51BrzNCla@cluster0.c723amg.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
 .then(() =>{console.log(`🟢 database started !`)})
 .catch(() =>{ console.log(`🔴 error connexion to database !`) })
